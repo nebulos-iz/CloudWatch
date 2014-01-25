bcca6c21920fcee54644437631b7bcc0d7ece21a
James Laskey <James_Laskey@bronw.edu>
2014-01-25 13:00:46 -0500

    updated compare with padding

--- matlab/compare.m
+++ matlab/compare.m
@@ -1,46 +1,34 @@
-function [confidence] = compare(BW1, x1, y1, orient1, BW2, x2 ,y2, orient2)
+function [confidence] = compare(BW1, x1, y1, orient1, BW2, x2 ,y2, orient2, color)
 %rotate images to match orientation
 rotate = orient2 - orient1;
 BW2 = imrotate(BW2, rotate);
 
-t = [1:10]
-y = center(t, 2, 4)
-
 BW1 = center(BW1, x1, y1); 
-BW2 = center(BW2, x2, y2); 
+BW2 = center(BW2, x2, y2);
+color2 = center(color, x2, y2);
 
-t = magic(10); 
-y = center(t, 2, 4)
+[rows1, cols2] = size(BW1);
+[rows2, cols2] = size(BW2);
 
+diff_rows = rows2 - rows1;
+diff_cols = cols2 - cols1;
 
-
-    function centered_img = center(img, x, y)
-        %center image 1: 
-        %center x
-        if (size(img, 2)/2 - x > 0) 
-            %offset is too far right; pad on right
-            pad = zeros([size(img, 1), (x - (size(img, 2) - x))]); 
-            img = horzcat(img, pad); 
-        else
-            %offset is too far left
-            dim = size(img, 2) - x; 
-            pad = zeros([size(img, 1), dim - x]); 
-            img = horzcat(pad, img); 
-        end
-        %center y
-        if (size(img, 1)/2 - y > 0) 
-            %center is too far down; pad on bottom
-            dim = size(img, 1) - y; 
-            pad = zeros([dim - y, size(img, 2)]); 
-            img = vertcat(img, pad);
-        else 
-            %center is too high up; pad on top
-            pad = zeros([y - (size(img, 1) - y), size(img, 2)]); 
-            img = vertcat(pad, img); 
-        end 
-        centered_img = img; 
-    end
+if(diff_rows > 0)
+    BW1 = padarray(BW1, [diff_rows/2 0]);
+else
+    diff_rows = 0 - diff_rows;
+    BW2 = padarray(BW2, [diff_rows/2 0]);
+end
 
 
+if(diff_cols > 0)
+    BW1 = padarray(BW1, [0 diff_cols/2]);
+else
+    diff_cols = 0 - diff_cols;
+    BW2 = padarray(BW2, [0 diff_cols/2])
+end
+
+for i = 1 : 5
+    
 
 end
