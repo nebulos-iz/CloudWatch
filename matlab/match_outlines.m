cb6342613894d3d46451c67d9a24f6bee8b6ed42
exwu <emily_wu@brown.edu>
2014-01-25 00:53:43 -0500

    updated match_outlines so that it now returns a binary image w/ fg labeled 1 and all else labeled 0. reading images also now works. Screw you too, MATLAB.

--- matlab/match_outlines.m
+++ matlab/match_outlines.m
@@ -10,26 +10,28 @@ function [matches] = match_outlines(cloud_outline)
 
 path = '/home/emily/Documents/MATLAB/1obj/';
 
-files = dir(path);
+files = dir(path)
 
 confidences = zeros([100]);
 counter = 1;
 
-for file = files'
-    fir = dir(file.name);
-    image = imread(fir(1).name);
-    red = image(:,1);
-    mask = im2bw(red, 1);
-    
-    [row_size, cols_size, ~] = size(mask);
-    if(row_size/col_size > 1.1)
-        mask = imcrop(mask, [0, 0, col_size, col_size]);
-    elseif(row_size/col_size < .9)
-        mask = imcrop(mask, [0, 0, row_size, row_size]);
+for folder = files'
+    %file random folder name
+    if (folder.name(1) == '.') 
+        continue; 
     end
-    mask = imresize(mask, [300, 300]);
+    folder_name = strcat(path, folder.name); 
+    name_file = dir(strcat(folder_name, '/*.name'));
+    name = name_file.name;
+    imgs = dir(strcat(folder_name, '/human_seg/*.png')); 
+    image = imread(strcat(folder_name, '/human_seg/', imgs(1).name));
+    imshow(image); 
     
-    counter = counter +1;
+    red = image(:, :,1); 
+    
+    mask = image(:, :, 1) == 255 & image(:, :, 2) == 0 & image(:, :, 3) == 0;
+    imshow(mask);
+ 
 end
 
 
