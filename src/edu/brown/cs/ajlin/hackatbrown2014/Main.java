c6ade3b8c651b973eaed7cb6fdef6fb021554944
Bitzeralisis <bitzeralisis@gmail.com>
2014-01-25 02:00:38 -0500

    Large amount of UI work

--- src/edu/brown/cs/ajlin/hackatbrown2014/Main.java
+++ src/edu/brown/cs/ajlin/hackatbrown2014/Main.java
@@ -4,20 +4,13 @@ import matlabcontrol.*;
 
 public class Main {
 
-	public static void main(String[] args) throws MatlabConnectionException, MatlabInvocationException
-	{
-	    //Create a proxy, which we will use to control MATLAB
-		MatlabProxyFactoryOptions options = new MatlabProxyFactoryOptions.Builder()
-													.setUsePreviouslyControlledSession(true)
-													.build();
-	    MatlabProxyFactory factory = new MatlabProxyFactory(options);
-	    MatlabProxy proxy = factory.getProxy();
-
-	    //evaluates the function doStuff
-	    proxy.eval("doStuff(2)");
-
-	    //Disconnect the proxy from MATLAB
-	    proxy.disconnect();
+	public static void main(String[] args) {
+		javax.swing.SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+            	new Cloudwatcher().begin();
+            }
+        });
 	}
+
 }
 
