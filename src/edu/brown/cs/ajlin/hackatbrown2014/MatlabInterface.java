f54898c78295f6cb791a65c8fd09ebf3b66aec31
Bitzeralisis <bitzeralisis@gmail.com>
2014-01-25 15:46:53 -0500

    Added Matlab interfacing

---
+++ src/edu/brown/cs/ajlin/hackatbrown2014/MatlabInterface.java
@@ -0,0 +1,24 @@
+package edu.brown.cs.ajlin.hackatbrown2014;
+
+import matlabcontrol.*;
+
+public class MatlabInterface {
+
+	private MatlabProxy proxy_;
+
+	public MatlabInterface() throws MatlabConnectionException {
+		MatlabProxyFactoryOptions options = new MatlabProxyFactoryOptions.Builder()
+							.setUsePreviouslyControlledSession(true)
+							.build();
+		MatlabProxyFactory factory = new MatlabProxyFactory(options);
+		proxy_ = factory.getProxy();
+	}
+	
+	public String cloudwatch(String cloudPath) throws MatlabInvocationException {
+		proxy_.eval("j_cloudwatch_ret = cloudWatch(" + cloudPath + ")");
+		Object retval = proxy_.getVariable("j_cloudwatch_ret");
+		System.out.println(retval);
+		return "assets/testimage.jpg";
+	}
+	
+}
