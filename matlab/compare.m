bbb628ebf68ef9b964c81a0d0d17e4595491788a
exwu <emily_wu@brown.edu>
2014-01-25 02:00:40 -0500

    making changes to compare s.t. it centers images first; may have broke stuff.

--- matlab/compare.m
+++ matlab/compare.m
@@ -1,28 +1,45 @@
-function [confidence] = compare(BW1, x1, y1, orient, BW2, x2 ,y2, orient2)
-
+function [confidence] = compare(BW1, x1, y1, orient1, BW2, x2 ,y2, orient2)
+%rotate images to match orientation
 rotate = orient2 - orient1;
 BW2 = imrotate(BW2, rotate);
-diff_x = x2 - x1;
-diff_rows = (size(BW2, 1) - y2) - (size(BW1,1) - y1);
+
+t = [1:10]
+y = center(t, 2, 4)
+
+BW1 = center(BW1, x1, y1); 
+BW2 = center(BW2, x2, y2); 
+
+t = magic(10); 
+y = center(t, 2, 4)
 
 
-%put the centroids at the same point
-if(diff_x < 0)
-    %need to pad BW2
-    pad = zeros(size(BW2,1), diff_x);
-    horzcat(pad, BW2);
-else
-    pad = zeros(size(BW1,1), diff_x);
-    horzcat(pad, BW1);
-end
 
-if(diff_rows < 0)
-    pad = zeros(size(BW2,2), diff_rows);
-    vertcat(pad, BW2);
-else
-    pad = zeros(size(BW1,2), diff_rows);
-    vertcat(pad, BW1);
-end
+    function centered_img = center(img, x, y)
+        %center image 1: 
+        %center x
+        if (size(img, 2)/2 - x > 0) 
+            %offset is too far right; pad on right
+            pad = zeros([size(img, 1), (x - (size(img, 2) - x))]); 
+            img = horzcat(img, pad); 
+        else
+            %offset is too far left
+            dim = size(img, 2) - x; 
+            pad = zeros([size(img, 1), dim - x]); 
+            img = horzcat(pad, img); 
+        end
+        %center y
+        if (size(img, 1)/2 - y > 0) 
+            %center is too far down; pad on bottom
+            dim = size(img, 1) - y; 
+            pad = zeros([dim - y, size(img, 2)]); 
+            img = vertcat(img, pad);
+        else 
+            %center is too high up; pad on top
+            pad = zeros([y - (size(img, 1) - y), size(img, 2)]); 
+            img = vertcat(pad, img); 
+        end 
+        centered_img = img; 
+    end
 
 
 
